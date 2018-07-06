14635a1c1035d3d936b973a42c980cee644dbd34
Git1 <Git1@veative.com>
2018-07-06 17:25:32 +0530

    Test Func

--- TestUnityProject/Assets/AVProVideo/Demos/Scripts/AutoRotate.cs
+++ TestUnityProject/Assets/AVProVideo/Demos/Scripts/AutoRotate.cs
@@ -9,34 +9,39 @@ namespace RenderHeads.Media.AVProVideo.Demos
 	/// <summary>
 	/// Rotates a transform, randomly changing direction and speed every few seconds
 	/// </summary>
-	[RequireComponent(typeof(Transform))]
+	[RequireComponent (typeof(Transform))]
 	public class AutoRotate : MonoBehaviour
 	{
 		private float x, y, z;
 		private float _timer;
 
-		private void Awake()
+		private void Awake ()
 		{
-			Randomise();
+			Randomise ();
 		}
 
-		private void Update()
+		private void Update ()
 		{
-			this.transform.Rotate(x * Time.deltaTime, y * Time.deltaTime, z * Time.deltaTime);
+			this.transform.Rotate (x * Time.deltaTime, y * Time.deltaTime, z * Time.deltaTime);
 			_timer -= Time.deltaTime;
-			if (_timer <= 0f)
-			{
-				Randomise();
+			if (_timer <= 0f) {
+				Randomise ();
 			}
 		}
 
-		private void Randomise()
+		private void Randomise ()
 		{
 			float s = 32f;
-			x = Random.Range(-s, s);
-			y = Random.Range(-s, s);
-			z = Random.Range(-s, s);
-			_timer = Random.Range(5f, 10f);
+			x = Random.Range (-s, s);
+			y = Random.Range (-s, s);
+			z = Random.Range (-s, s);
+			_timer = Random.Range (5f, 10f);
+		}
+
+
+		void TestFunc ()
+		{
+		
 		}
 	}
 }
